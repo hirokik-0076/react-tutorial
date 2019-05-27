f68e43cef8741d1c68438e9b7da936fecf917765
Hiroki Kinoshita <hiroki.kinoshita@sun-asterisk.com>
2019-05-27 09:51:42 +0700

    tic-tac-toe feature

--- src/index.js
+++ src/index.js
@@ -4,26 +4,12 @@ import './index.css';
 
 //squareは正方形のマス目　クラスコンポーネント
 //子コンポーネント
-class Square extends React.Component {
-  //steteを使用するためにコンストラクタを定義する。
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-  render() {
-    //Square（マス目）コンポーネントは 1 つの <button> をレンダー
-    return (
-      <button
-        className="square"
-        //ここで状態を保存している
-        onClick={() => this.setState({ value: 'X' })}
-      >
-        {this.state.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 //盤面のクラスコンポーネント
 //BoardコンポーネントはSquareコンポーネントの親コンポーネント
@@ -31,8 +17,22 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      square: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    };
+  }
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
     }
+    //真偽値が反転されXかOが表示される様になっている
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      //setStateでxIsNextの真偽値を反転させている。
+      xIsNext: !this.state.xIsNext,
+    });
   }
   //Board（盤面）が 9 個のマス目をレンダー
   renderSquare(i) {
@@ -47,7 +47,16 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    //勝者を定義
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      //勝者を記述
+      status = 'Winner: ' + winner;
+    } else {
+      //playerの手番を表示
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -89,10 +98,29 @@ class Game extends React.Component {
     );
   }
 }
-
 // ========================================
 
 ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+//勝者を決める関数
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
