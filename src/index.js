f19c072282a356867cdacebdbd7ca163c21cc8bb
Hiroki Kinoshita <hiroki.kinoshita@sun-asterisk.com>
2019-05-27 15:44:22 +0700

    time travel feature add

--- src/index.js
+++ src/index.js
@@ -14,53 +14,22 @@ function Square(props) {
 //盤面のクラスコンポーネント
 //BoardコンポーネントはSquareコンポーネントの親コンポーネント
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    //真偽値が反転されXかOが表示される様になっている
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      //setStateでxIsNextの真偽値を反転させている。
-      xIsNext: !this.state.xIsNext,
-    });
-  }
+  //現時点で Board コンポーネントに必要なのは renderSquare と render メソッドだけです。ゲームの状態と handleClick メソッドは Game コンポーネント内にあります。
   //Board（盤面）が 9 個のマス目をレンダー
   renderSquare(i) {
     //propsとしてvalueという名前の値をSquareに渡す様にしている
     return (
       <Square
-        value={this.state.squares[i]}
+        value={this.props.squares[i]}
         //マス目をクリックした時に呼ばれる関数
-        onClick={() => this.handleClick(i)}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
 
   render() {
-    //勝者を定義
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      //勝者を記述
-      status = 'Winner: ' + winner;
-    } else {
-      //playerの手番を表示
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -83,16 +52,87 @@ class Board extends React.Component {
 
 //Gameのクラスコンポーネント
 class Game extends React.Component {
+  //Gameのコンストラクタで定義したものをBordに渡している
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      //jumpToメソッドで使用するstepNumber
+      stepNumber: 0,
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i) {
+    //履歴エントリhistoryを追加
+    //「時間の巻き戻し」をしてからその時点で新しい着手を起こした場合に、そこから見て「将来」にある履歴（もはや正しくなくなったもの）を確実に捨て去ることができます。
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      //履歴の状態を時系列ごとに追加
+      history: history.concat([{
+        squares: squares,
+      }]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+  //jumTo methodを定義
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    });
+  }
+
   render() {
-    //Game コンポーネントは盤面と、後ほど埋めることになるプレースホルダーを描画しています。
+    //履歴
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    //勝者を定義
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      )
+    })
+
+    //Bordに渡す
+    let status;
+    if (winner) {
+      //勝者を記述
+      status = 'Winner: ' + winner;
+    } else {
+      //playerの手番を表示
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
+
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
@@ -100,10 +140,7 @@ class Game extends React.Component {
 }
 // ========================================
 
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
-);
+ReactDOM.render(<Game />, document.getElementById("root"));
 //勝者を決める関数
 function calculateWinner(squares) {
   const lines = [
